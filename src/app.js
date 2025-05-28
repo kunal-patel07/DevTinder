f04935104b969177111783731d1c867f40f1c415
kunal-patel07 <kunnalpatel2004@gmail.com>
2025-05-28 17:58:40 +0530

    added multiple route handler in one

--- src/app.js
+++ src/app.js
@@ -1,31 +1,35 @@
-const express= require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
 const port = 3000;
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("first route");
+    next();
+  },
+  (req, res) => {
+    console.log("2nd route");
+    res.send("this is inside route");
+  }
+);
+// app.post("/user",(req,res)=>{
+//    res.send("user data stored to database")
+// })
 
+// app.delete("/user",(req,res)=>{
+//       res.send("user deleted successful")
+// })
 
+// app.get("/test",(req,res)=>{
+//     res.send("this is for testing")
+// })
 
-app.post("/user",(req,res)=>{
-   res.send("user added successfully")
-})
+// app.get("/",(req,res)=>{
+//   res.send("this is route page")
+// })
 
-app.get("/user",(req,res)=>{
-    res.send("user detail")
-})
-
-app.delete("/user",(req,res)=>{
-      res.send("user deleted successful")
-})
-
-app.get("/test",(req,res)=>{
-    res.send("this is for testing")
-})
-
-app.get("/",(req,res)=>{
-  res.send("this is route page")
-})
-
-app.listen(port,()=>{
-    console.log("port is listening on ",port)
-})
+app.listen(port, () => {
+  console.log("port is listening on ", port);
+});
