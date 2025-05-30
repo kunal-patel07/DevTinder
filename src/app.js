08b9c4920d85ddde814a2bb312555a24f98b4c03
kunal-patel07 <kunnalpatel2004@gmail.com>
2025-05-30 17:29:01 +0530

    created delete api & feed that have all the doc of db &created get findbyEmailid

--- src/app.js
+++ src/app.js
@@ -1,31 +1,65 @@
 const express = require("express");
-const {dbConn}  = require("./config/database")
-const {User} = require("./models/user")
+const { dbConn } = require("./config/database");
+const { User } = require("./models/user");
 const port = 3000;
 
-let app = express()
+let app = express();
 
 //kind of middleware
-app.use(express.json())
-app.post("/signup",async (req,res)=>{
+app.use(express.json());
 
-      let user = new User(req.body)
-      try {
-        await user.save()
-        res.send("user saved to database sucessfully")
-        
-      } catch (error) {
-        console.log("error storing data")
-      }
+app.get("/user",async(req,res)=>{
+  const emailfromServer = req.body.emailId;
+   try {
+    const getEmailfromDb= await User.find({emailId : emailfromServer})
+    if(!getEmailfromDb){
+      res.status(404).send("something went wrong");
+    }else{
+      res.send(getEmailfromDb)
+    }
+   } catch (error) {
+          res.status(404).send("something went wrong");
+   }
 })
 
 
-
-dbConn().then(()=>{
-  console.log("connected to database")
-  app.listen(port,()=>{
-       console.log("server is running on",port)
-  },port)
-}).catch(()=>{
-  console.log("error in connection`")
+app.get("/feed",async(req,res)=>{
+  const data = await User.find({})
+  res.send(data)
 })
+
+app.delete("/user",async(req,res)=>{
+  const userfromServer= req.body.userId;
+  try {
+    const userDelete = await User.findByIdAndDelete(userfromServer)
+    res.send("user deleted successfully")
+  } catch (error) {
+    res.status(404).send("error deleting user")
+  }
+})
+
+
+app.post("/signup", async (req, res) => {
+  let user = new User(req.body);
+  try {
+    await user.save();
+    res.send("user saved to database sucessfully");
+  } catch (error) {
+    console.log("error storing data");
+  }
+});
+
+dbConn()
+  .then(() => {
+    console.log("connected to database");
+    app.listen(
+      port,
+      () => {
+        console.log("server is running on", port);
+      },
+      port
+    );
+  })
+  .catch(() => {
+    console.log("error in connection");
+  });
