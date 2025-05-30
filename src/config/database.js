b6c89e32491d9a98dd4666c34ef5201c582460a6
kunal-patel07 <kunnalpatel2004@gmail.com>
2025-05-30 10:36:56 +0530

    secured database api

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
+require("dotenv").config()
 const mongoose = require("mongoose")
 
 
+
 const connectDb = async()=>{
-    await mongoose.connect("mongodb+srv://krunal:hY1W2PpaghR2lxYV@namstenode.i9hyhjt.mongodb.net/devTinder")
+    await mongoose.connect(process.env.DB_API_KEY)
 }
 module.exports ={connectDb}
