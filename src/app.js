4dd8bc38189f21677d66cce07be0513200e7e0e0
kunal-patel07 <kunnalpatel2004@gmail.com>
2025-06-02 22:27:13 +0530

    practiced api creation

--- src/app.js
+++ src/app.js
@@ -9,20 +9,25 @@ let app = express();
 app.use(express.json());
 
 app.get("/user",async(req,res)=>{
-  const emailfromServer = req.body.emailId;
-   try {
-    const getEmailfromDb= await User.find({emailId : emailfromServer})
-    if(!getEmailfromDb){
-      res.status(404).send("something went wrong");
-    }else{
-      res.send(getEmailfromDb)
-    }
-   } catch (error) {
-          res.status(404).send("something went wrong");
-   }
+  const emailFromServer = req.body.emailId;
+  try {
+    const emailFromDb = await User.find({emailId:emailFromServer})
+  if(!emailFromDb){
+    res.status(404).send("cant find it")
+  }else{
+    res.send(emailFromDb)
+  }
+
+  } catch (error) {
+   res.status(404).send("cant find it")
+  }
 })
 
-
+app.post("/",async (req,res)=>{
+ const datafromServer  = new User(req.body)
+  await datafromServer.save()
+ res.send("user registred")
+})
 app.get("/feed",async(req,res)=>{
   const data = await User.find({})
   res.send(data)
@@ -30,14 +35,19 @@ app.get("/feed",async(req,res)=>{
 
 app.delete("/user",async(req,res)=>{
   const userfromServer= req.body.userId;
+  console.log(userfromServer)
   try {
-    const userDelete = await User.findByIdAndDelete(userfromServer)
+   const userDelete = await findByIdAndDelete(userfromServer)
+if(!userDelete){
+  res.status(404).send("user doesn't exist or deleted")
+}else{
     res.send("user deleted successfully")
+}
+  
   } catch (error) {
-    res.status(404).send("error deleting user")
+    res.status(404).send("error deleting user or userid is not exist")
   }
-})
-
+})  
 
 app.post("/signup", async (req, res) => {
   let user = new User(req.body);
@@ -48,6 +58,30 @@ app.post("/signup", async (req, res) => {
     console.log("error storing data");
   }
 });
+
+app.post("/userfromServer", async(req,res)=>{
+  const data  = new User(req.body );
+
+try {
+   await data.save();
+   res.send("data is saved successfully")
+} catch (error) {
+res.status(401).send("something went wrong")
+  }
+})
+
+app.get("/findByMail",async (req,res)=>{
+  const mailFromServer  = req.body.firstName
+  try {
+   const dataFromDb = await User.findOne({firstName:mailFromServer})
+   res.send(dataFromDb)
+  } catch (error) {
+    
+  }
+})
+
+
+
 
 dbConn()
   .then(() => {
