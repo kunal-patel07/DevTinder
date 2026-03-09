0ac27d2cd40973982f7fbdefc3b0e62782f6cd6d
kunal-patel07 <kunnalpatel2004@gmail.com>
2026-03-09 16:27:34 +0530

    playing with api,dynamic routing

--- src/app.js
+++ src/app.js
@@ -1,33 +1,18 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.delete("/user",(req,res)=>{
-    res.send("user data deleted")
-}) 
-
- // accept every http reqest 
-// app.use("/user",(req,res)=>{
-//     res.send("user data will same for everyone")
-// })
-
-app.patch("/user",(req,res)=>{
-    res.send("user data updated")
-}) 
-
-app.post("/user",(req,res)=>{
-    res.send({firstName : "Krunal" , lastName : "Patel"})
-}) 
-app.get("/user",(req,res)=>{
-    res.send("user got get request")
-}) 
-
-app.use("/test",(req,res)=>{
-    res.send("this is basic server")
-    console.log("every time you hit")
-})
+    const express = require("express");
+    const app = express();
+    const port = 3000;
 
 
-app.listen(port,()=>{
-    console.log(` server is running on ${port} `)
-})
+    app.get("/user/:id",(req,res)=>{
+        const id = req.params.id;
+        console.log(id)
+        res.send(`user got get request ${id}`)
+    }) 
+
+
+
+
+
+    app.listen(port,()=>{
+        console.log(` server is running on ${port} `)
+    })
