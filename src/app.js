85ebd1ec9f3e935b5a1ce509b101f69708b1d6f5
kunal-patel07 <kunnalpatel2004@gmail.com>
2026-03-09 15:49:43 +0530

    exploring get,post,delete

--- src/app.js
+++ src/app.js
@@ -2,10 +2,31 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/",(req,res)=>{
+app.delete("/user",(req,res)=>{
+    res.send("user data deleted")
+}) 
+
+ // accept every http reqest 
+// app.use("/user",(req,res)=>{
+//     res.send("user data will same for everyone")
+// })
+
+app.patch("/user",(req,res)=>{
+    res.send("user data updated")
+}) 
+
+app.post("/user",(req,res)=>{
+    res.send({firstName : "Krunal" , lastName : "Patel"})
+}) 
+app.get("/user",(req,res)=>{
+    res.send("user got get request")
+}) 
+
+app.use("/test",(req,res)=>{
     res.send("this is basic server")
     console.log("every time you hit")
 })
+
 
 app.listen(port,()=>{
     console.log(` server is running on ${port} `)
