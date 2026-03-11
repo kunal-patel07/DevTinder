9eb8608ba35e51db93a5bf9c771c90d801c1464b
kunal-patel07 <kunnalpatel2004@gmail.com>
2026-03-11 19:16:53 +0530

    database-basics

--- src/app.js
+++ src/app.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const connectDb = require("./config/database");
+const User = require("./models/user");
 
-app.use("/user",(req,res)=>{
-    throw new Error("something went wrong");    
-})
-app.use("/",(err,req,res,next)=>{
-    if(err){
-      res.status(404).send("new error occured conact support team")
-      console.log("err handled by express err handled")
-    }
-})
-
-
-app.listen(port, () => {
-  console.log(` server is running on ${port} `);
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Michael",
+    lastName: "Phelps",
+    emailId: "michel@phelps.com",
+    password: "mp@swim",
+  });
+  try {
+    await user.save();
+    res.send("user data saved successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user" + err);
+  }
 });
+
+connectDb()
+  .then(() => {
+    console.log("database connected successfully");
+
+    app.listen(port, () => {
+      console.log(`server is running on ${port} `);
+    });
+  })
+  .catch((err) => {
+    console.log("error in connection", err.message);
+  });
