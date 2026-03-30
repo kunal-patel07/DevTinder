887a535cd9c7a45ec1e87251361e0d20826cd293
kunal-patel07 <kunnalpatel2004@gmail.com>
2026-03-30 19:52:24 +0530

    mafe feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const connectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -46,40 +47,38 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
     }
   });
 
-  res.json({data:data});
+  res.json({ data: data });
 });
 
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
 
+    const connectionRequests = await connectionRequestModel
+      .find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      })
+      .select("fromuserId toUserId");
 
+    const hideUserFromFeed = new Set();
 
+    connectionRequests.forEach((request) => {
+    hideUserFromFeed.add(request.fromUserId)
+    hideUserFromFeed.add(request.toUserId)
+    });
 
-// userRouter.get("/user/connection", userAuth, async (req, res) => {
-//   const loggedInUser = req.user;
+    console.log(hideUserFromFeed)
+    const user = await User.find({
+      $and : [
+      {_id : {$nin : Array.from(hideUserFromFeed)}},
+      {_id : {$ne : loggedInUser._id}}
+      ]
+    }).select(["firstName", "lastName", "photoUrl", "skills"])
 
-//   const findConnections = await connectionRequestModel
-//     .find({
-//       $and: [
-//         { status: "accepted" },
-//         {
-//           $or: [
-//             { fromUserId: loggedInUser._id },
-//             { toUserId: loggedInUser._id },
-//           ],
-//         },
-//       ],
-//     })
-//     .populate("fromUserId", ["firstName", "lastName", "photoUrl", "skills"])
-//     .populate("toUserId", ["firstName", "lastName", "photoUrl", "skills"]);
-
-//   const data = findConnections.map((field) => {
-//     if (field.fromUserId._id.toString() === loggedInUser._id.toString()) {
-//       return field.toUserId;
-//     } else {
-//       return field.fromUserId;
-//     }
-//   });
-
-//   res.json(data);
-// });
+    res.json(user)
+  } catch (error) {
+    res.status(400).send("ERROR :" + error.message);
+  }
+});
 
 module.exports = userRouter;
